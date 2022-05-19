840eab50928c167243fd54b56db96fc221a02aca
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-05-18 22:42:58 -0400

    finished food delivery app

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import classes from './Cart.module.css'
 import Modal from '../UI/Modal'
-
+import CartContext from '../../store/cart-context'
+import CartItem from './CartItem'
 
 const Cart = props => {
+    const cartContext = useContext(CartContext)
+
+    const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`
+    const hasItems = cartContext.items.length > 0
+
+
+
+    const cartItemRemoveHandler = id => {
+        cartContext.removeItem(id)
+    }
+
+
+    const cartItemAddHandler = item => {
+        cartContext.addItem({...item, amount:1})
+    }
+
     const cartItems = (
         <ul className={classes['cart-items']}>
-            {[{ id: 'c1', name: 'sushi', amount: 2, price: 12.99 }].map((item) => (
-                <li>{item.name}</li>
+            {cartContext.items.map((item) => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
             ))}
         </ul>
     )
@@ -18,7 +42,7 @@ const Cart = props => {
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button
@@ -27,7 +51,7 @@ const Cart = props => {
                 >
                     Close
                 </button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     )
